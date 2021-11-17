b542f7b7038f5f2094524a145aeecf22608d217a
david <chursa99192@gmail.com>
2021-11-17 20:03:33 +0200

    add service layer

---
+++ src/main/java/com/store/services/ProductService.java
@@ -0,0 +1,18 @@
+package com.store.services;
+
+import com.store.entities.Product;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductService{
+    List<Product> getAll();
+
+    boolean delete(Product object);
+
+    Optional<Product> getById(int id);
+
+    boolean save(Product object);
+
+    boolean update(Product object);
+}
---
+++ src/main/java/com/store/services/ProductServiceImpl.java
@@ -0,0 +1,39 @@
+package com.store.services;
+
+import com.store.entities.Product;
+import com.store.repositories.ProductRepository;
+import java.util.List;
+import java.util.Optional;
+
+public class ProductServiceImpl implements ProductService{
+    private final ProductRepository repository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.repository = productRepository;
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return repository.getAll();
+    }
+
+    @Override
+    public boolean delete(Product object) {
+        return repository.delete(object);
+    }
+
+    @Override
+    public Optional<Product> getById(int id) {
+        return repository.getById(id);
+    }
+
+    @Override
+    public boolean save(Product object) {
+        return repository.save(object);
+    }
+
+    @Override
+    public boolean update(Product object) {
+        return repository.update(object);
+    }
+}
