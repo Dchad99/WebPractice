3b0c5e9afa00559208ea3d53614c50b21c2fc297
david <chursa99192@gmail.com>
2021-11-17 19:59:15 +0200

    add crud repository

---
+++ src/main/java/com/store/repositories/CrudRepository.java
@@ -0,0 +1,16 @@
+package com.store.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CrudRepository<T> {
+    List<T> getAll();
+
+    boolean delete(T t);
+
+    Optional<T> getById(int id);
+
+    boolean save(T t);
+
+    boolean update(T t);
+}
