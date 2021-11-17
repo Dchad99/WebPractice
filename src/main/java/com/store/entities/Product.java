d9d456cc0c0b61d0a0eeebb87b592b07f9d27fd0
david <chursa99192@gmail.com>
2021-11-17 20:07:42 +0200

    add entity layer,  Product

--- src/main/java/com/store/entities/Product.java
+++ src/main/java/com/store/entities/Product.java
@@ -1,12 +1,55 @@
 package com.store.entities;
 
-import lombok.Data;
-import java.time.LocalDate;
+import com.store.repositories.queries.Column;
+import com.store.repositories.queries.Entity;
+import com.store.repositories.queries.Id;
+import java.sql.Date;
 
-@Data
+
+@Entity(table = "products")
 public class Product {
+    @Id
     private int id;
+
+    @Column
     private String name;
+
+    @Column
     private int price;
-    private LocalDate date;
+
+    @Column
+    private Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
 }
