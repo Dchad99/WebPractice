910ea38449d7bc68f160bf0482ec26e6c6d24a46
david <chursa99192@gmail.com>
2021-11-23 03:43:06 +0200

    add check if user authorized or not

---
+++ src/main/java/com/store/controllers/AddProductServlet.java
@@ -0,0 +1,61 @@
+package com.store.controllers;
+
+import com.store.entities.Product;
+import com.store.services.ProductService;
+import lombok.SneakyThrows;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class AddProductServlet extends HttpServlet {
+    private final ProductService service;
+
+    public AddProductServlet(ProductService service) {
+        this.service = service;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        boolean isAuth = false;
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for (Cookie cookie : request.getCookies()) {
+                if(cookie.getName().equalsIgnoreCase("user-token")){
+                    isAuth = true;
+                }
+            }
+        }
+
+        if(isAuth) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getOutputStream().write(this.getClass()
+                    .getClassLoader()
+                    .getResourceAsStream("templates/addProduct.html")
+                    .readAllBytes());
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.sendRedirect("/login");
+        }
+    }
+
+    @Override
+    @SneakyThrows
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        Product product = new Product();
+        product.setName(request.getParameter("name"));
+        product.setPrice(Integer.parseInt(request.getParameter("price")));
+        product.setDate(new Date());
+
+        boolean status = service.save(product);
+
+        String message = !status ? "Product wasn't added" : "Product was added";
+        response.setStatus(HttpServletResponse.SC_CREATED);
+        response.sendRedirect("/products");
+        response.getWriter().write(message);
+    }
+
+}
