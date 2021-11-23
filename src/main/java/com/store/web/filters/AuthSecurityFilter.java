160125cf51ce50ff89f745ae34a4c8f6a4c8e40b
david <chursa99192@gmail.com>
2021-11-24 00:51:08 +0200

    add filter layer e extract logic from servlets that stands for checking cookies data for user-token property existence

---
+++ src/main/java/com/store/web/filters/AuthSecurityFilter.java
@@ -0,0 +1,43 @@
+package com.store.web.filters;
+
+import lombok.extern.slf4j.Slf4j;
+import javax.servlet.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class AuthSecurityFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("Filter start working");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+
+        boolean isAuth = false;
+        Cookie[] cookies = httpServletRequest.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : httpServletRequest.getCookies()) {
+                if (cookie.getName().equalsIgnoreCase("user-token")) {
+                    isAuth = true;
+                }
+            }
+        }
+
+        if (isAuth) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else {
+            httpServletResponse.sendRedirect("/login");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        log.info("Filter stop working");
+    }
+}
