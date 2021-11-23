dca1489ec18c97c37eb35badabe07af9ff2469e9
david <chursa99192@gmail.com>
2021-11-23 19:38:47 +0200

    add setter for product description

--- src/main/java/com/store/controllers/UpdateProductServlet.java
+++ src/main/java/com/store/controllers/UpdateProductServlet.java
@@ -51,8 +51,8 @@ public class UpdateProductServlet extends HttpServlet {
             p.setName(request.getParameter("name"));
             p.setPrice(Integer.parseInt(request.getParameter("price")));
             p.setDate(new Date());
+            p.setDescription(request.getParameter("desc"));
             service.update(p);
-            response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
             response.sendRedirect("/products");
         } else {
             response.getWriter().write("Sorry, but story doesn't contain product with such ID: " + request.getParameter("id"));
