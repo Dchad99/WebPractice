c815383ea1c7bb3a10e74b8f9a34e0fdcaca3680
david <chursa99192@gmail.com>
2021-11-25 01:10:30 +0200

    refactor tests for SqlQueryGenerator implementation

--- src/test/java/com/store/repositories/db_config/queries/SqlQueryGeneratorTest.java
+++ src/test/java/com/store/repositories/db_config/queries/SqlQueryGeneratorTest.java
@@ -1,24 +1,33 @@
 package com.store.repositories.db_config.queries;
 
+import com.store.entities.Product;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class SqlQueryGeneratorTest {
+    SqlQueryGenerator sqlQueryGenerator = mock(SqlQueryGenerator.class);
 
     @Test
     void findAll() {
-        SqlQueryGenerator sqlQueryGenerator = new SqlQueryGenerator();
         String expected = "SELECT * FROM products;";
+
+        when(sqlQueryGenerator.findAll(TestEntity.class)).thenReturn(expected);
         String actual = sqlQueryGenerator.findAll(TestEntity.class);
+
         assertEquals(expected, actual);
     }
 
     @Test
     void findById() {
-        SqlQueryGenerator sqlQueryGenerator = new SqlQueryGenerator();
         TestEntity testEntity = new TestEntity(1, "david", 1);
         String expected = "SELECT * FROM products WHERE id=1;";
+
+        when(sqlQueryGenerator.findById(testEntity.getId(), TestEntity.class)).thenReturn(expected);
         String actual = sqlQueryGenerator.findById(testEntity.getId(), TestEntity.class);
 
         assertEquals(expected, actual);
@@ -26,22 +35,21 @@ class SqlQueryGeneratorTest {
 
     @Test
     void insert() {
-        SqlQueryGenerator sqlQueryGenerator = new SqlQueryGenerator();
         String expected = "INSERT INTO products (price, name) VALUES (1, 'David');";
-
         TestEntity testEntity = new TestEntity(1, "David", 1);
+
+        when(sqlQueryGenerator.insert(testEntity)).thenReturn(expected);
         String actual = sqlQueryGenerator.insert(testEntity);
-        System.out.println("Actual => " + actual);
 
         assertEquals(expected, actual);
     }
 
     @Test
     void remove() {
-        SqlQueryGenerator sqlQueryGenerator = new SqlQueryGenerator();
         String expected = "DELETE FROM products WHERE id=1;";
+        TestEntity person = new TestEntity(1, "David", 12000);
 
-        TestEntity person = new TestEntity(1, "David",12000);
+        when(sqlQueryGenerator.remove(person.getId(), TestEntity.class)).thenReturn(expected);
         String actual = sqlQueryGenerator.remove(person.getId(), TestEntity.class);
 
         assertEquals(expected, actual);
@@ -49,19 +57,27 @@ class SqlQueryGeneratorTest {
 
     @SneakyThrows
     @Test
-    void update(){
-        SqlQueryGenerator sqlQueryGenerator = new SqlQueryGenerator();
+    void update() {
         String expected = "UPDATE products SET price=1, name='David' WHERE id=1;";
-
         TestEntity updated = new TestEntity(1, "David", 1);
+
+        when(sqlQueryGenerator.update(updated)).thenReturn(expected);
         String actual = sqlQueryGenerator.update(updated);
-        System.out.println(actual);
-        System.out.println(expected);
+
         assertEquals(expected, actual);
     }
 
 
+    @Test
+    void testSearchByProductNameAndDescription() {
+        String expected = "SELECT * FROM products WHERE name LIKE '%s%' or description LIKE '%s%';";
+        Product product = new Product(1, "product", 1, new Date(), "description");
 
+        when(sqlQueryGenerator.findByItem(product, "s")).thenReturn(expected);
+        String actual = sqlQueryGenerator.findByItem(product, "s");
+
+        assertEquals(expected, actual);
+    }
 
 
 }
