0f664264e918d2bfb307d1416224d01be17e49c0
david <chursa99192@gmail.com>
2021-11-30 03:01:45 +0200

    update logic of LoginServlet

--- src/main/java/com/store/web/servlets/LoginServlet.java
+++ src/main/java/com/store/web/servlets/LoginServlet.java
@@ -3,15 +3,9 @@ package com.store.web.servlets;
 import com.store.entities.User;
 import com.store.security.SecurityService;
 import com.store.services.UserService;
-import com.store.web.util.ResourceParser;
 import lombok.extern.slf4j.Slf4j;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.Optional;
@@ -32,13 +26,19 @@ public class LoginServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        final Optional<User> user = service.getByParam(username);
+
+        HttpSession session = request.getSession();
+        Optional<User> user = service.getByParam(username);
+
         if (user.isPresent()) {
             User fromDb = user.get();
             final String encoded_password = securityService.encryptData(password + fromDb.getUserHash());
             if (Objects.equals(encoded_password, fromDb.getPassword())) {
                 Cookie cookie = new Cookie("user-token", fromDb.getUserHash());
                 cookie.setMaxAge(15000);
+
+                session.setAttribute("user", fromDb);
+
                 response.addCookie(cookie);
                 response.sendRedirect("/web-store/products");
             }
