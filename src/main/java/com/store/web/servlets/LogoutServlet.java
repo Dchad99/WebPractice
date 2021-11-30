d467b1eac915abf258d31fbe5eb119583a3dd5ba
david <chursa99192@gmail.com>
2021-11-30 03:01:30 +0200

    update logic of LogoutServlet

--- src/main/java/com/store/web/servlets/LogoutServlet.java
+++ src/main/java/com/store/web/servlets/LogoutServlet.java
@@ -2,16 +2,14 @@ package com.store.web.servlets;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
         Cookie readCookie = null;
         Cookie[] cookies = req.getCookies();
         for (Cookie cookie : cookies) {
@@ -21,6 +19,9 @@ public class LogoutServlet extends HttpServlet {
                 readCookie = cookie;
             }
         }
+
+        session.invalidate();
+
         resp.addCookie(readCookie);
         resp.sendRedirect(req.getContextPath() + "/welcome");
     }
