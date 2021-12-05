a363f9fd946afba9dfbb45152de956e900a129a3
david <chursa99192@gmail.com>
2021-12-05 02:48:27 +0200

    add domain layer to ApplicationContext

---
+++ src/main/java/com/store/ioc/domain/Bean.java
@@ -0,0 +1,9 @@
+package com.store.ioc.domain;
+
+import lombok.Data;
+
+@Data
+public class Bean {
+    private String id;
+    private Object value;
+}
---
+++ src/main/java/com/store/ioc/domain/BeanDefinition.java
@@ -0,0 +1,13 @@
+package com.store.ioc.domain;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class BeanDefinition {
+    private String id;
+    private String className;
+    private Map<String, String> valueDependencies;
+    private Map<String, String> refDependencies;
+}
