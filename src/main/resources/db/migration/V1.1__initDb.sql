7ca0408ba87a8a3bbfdc21fe0f890e40a37586cd
david <chursa99192@gmail.com>
2021-12-05 02:28:33 +0200

    add unique script that aimed to create tables => products, users and relation between them orders, and fill product with data

---
+++ src/main/resources/db/migration/V1.1__initDb.sql
@@ -0,0 +1,39 @@
+CREATE TABLE products
+(
+    id    serial PRIMARY KEY,
+    name  VARCHAR(50) UNIQUE NOT NULL,
+    price Integer        NOT NULL,
+    date  TIMESTAMP          NOT NULL,
+    description VARCHAR(250)
+);
+
+CREATE TABLE users
+(
+    id serial PRIMARY KEY,
+    username  VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(250) NOT NULL,
+    userHash VARCHAR(250) NOT NULL
+);
+
+insert into products(name, price, date, description) values ('Milk', 120 , '2021-01-01', 'Burionka');
+insert into products(name, price, date, description) values ('Tomat', 420 , '2022-02-03', 'Red');
+insert into products(name, price, date, description) values ('MeatChicken', 1220 , '2022-02-03', 'Chicken');
+insert into products(name, price, date, description) values ('MeatGoat', 4310 , '2022-05-05', 'Goat');
+insert into products(name, price, date, description) values ('MeatCow', 4310 , '2022-08-02', 'Cow');
+
+
+drop table orders;
+CREATE TABLE orders
+(
+    id         serial PRIMARY KEY,
+    clientId    INTEGER NOT NULL,
+    productId INTEGER NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY (clientId)
+            REFERENCES users (id)
+            ON DELETE CASCADE,
+    CONSTRAINT fk_product
+        FOREIGN KEY (productId)
+            REFERENCES products (id)
+            ON DELETE CASCADE
+);
