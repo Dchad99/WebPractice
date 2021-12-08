9cf1692becf99aab86639c8da659ee15664f70f4
david <chursa99192@gmail.com>
2021-12-09 01:04:12 +0200

    remove unused classes related to ioc container

--- src/main/java/com/store/ioc/reader/BeanDefinitionReader.java
+++
@@ -1,8 +0,0 @@
-package com.store.ioc.reader;
-
-import com.store.ioc.domain.BeanDefinition;
-import java.util.List;
-
-public interface BeanDefinitionReader {
-    List<BeanDefinition> readBeanDefinitions();
-}
